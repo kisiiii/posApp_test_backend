a376a77c499bcb98f0f6eb3a09544f09ad231575
kishi_masahiro <ksmshr1988@gmail.com>
2025-03-21 16:47:53 +0900

    データ挿入コード修正

--- db/insert_data.py
+++ db/insert_data.py
@@ -1,17 +1,15 @@
-#csvファイルに保存したデータをデータベースのProductテーブルに挿入
+#python -m db.insert_data で実行してください。直接実行するとインポートエラーがでます
 
 import csv
 from sqlalchemy.orm import Session
-from .mymodels import Product
+from .mymodels import Product, Tax
 from .database import engine
-
-# CSVファイルのパス
-csv_file_path = "vegetable_products_jp.csv"
 
 # データベースへのセッションを作成
 session = Session(bind=engine)
 
-# CSVファイルを開いて読み込む
+# CSVデータをDBに挿入
+csv_file_path = "vegetable_products_jp.csv"
 with open(csv_file_path, mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
@@ -23,9 +21,16 @@ with open(csv_file_path, mode='r', encoding='utf-8') as file:
         )
         # データベースに追加
         session.add(product)
-
 # データをコミットして保存
+session.commit()
+
+
+# 消費税情報をDBに追加
+new_tax = Tax(ID=1, CODE=10, NAME="10%", PERCENT=0.10)
+session.add(new_tax)
 session.commit()
 
 # セッションを閉じる
 session.close()
+
+print("データが挿入されました。")
